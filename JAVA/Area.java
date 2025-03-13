1c68a720dbfef90e62648c3191ae2ebefbf9c40e
Sanjay Lakshmanan <rajasanjay21@gmail.com>
2025-03-13 18:11:39 +0530

    delete

--- JAVA/Area.java
+++
@@ -1,8 +0,0 @@
-public class Area{
-public static void main(String[] args) {
-Circle circle = new Circle(6);
-System.out.println("Area of Circle: " + circle.calculateArea());
-Rectangle rectangle = new Rectangle(4,6);
-System.out.println("Area of Rectangle: " + rectangle.calculateArea());
-}
-}
